f1ab2d36112b33aeed0c01049fbe5a5b8526a06f
Bryan Vera <brayanandresveragar@gmail.com>
2022-10-20 16:20:14 -0500

    :art: Inicializacion del proyecto
    Construccion base del proyecto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const months = document.getElementById("months")
+const days = document.getElementById("days")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+
+setInterval(()=>{
+    console.log("Ha pasado un segundo");
+},1000)
